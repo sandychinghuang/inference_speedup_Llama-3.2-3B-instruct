9391309edc5907262882ebadcaaf74b309fea4b1
sandychinghuang <sandyching0415@gmail.com>
2025-06-02 14:23:25 +0800

    update

--- quant_cfg.py
+++ quant_cfg.py
@@ -1,26 +1,9 @@
 from hqq.core.quantize import BaseQuantizeConfig
 
-# TODO: Make your own quant config for DeiT-S
-def get_quant_config_deit(model):
-    quant_config = {}
-    
-    # n_blocks = len(model.blocks)
-    # q2_config = BaseQuantizeConfig(nbits=2, group_size=32)
-    
-    # for i in range(n_blocks):
-    #     quant_config[f'blocks.{i}.attn.qkv'] = q2_config
-    #     quant_config[f'blocks.{i}.attn.proj'] = q2_config
-    #     quant_config[f'blocks.{i}.mlp.fc1'] = q2_config
-    #     quant_config[f'blocks.{i}.mlp.fc2'] = q2_config
-        
-    return quant_config
-
-# TODO: Make your own quant config for Language Model
 def get_quant_config_slm(model):
     quant_config = {}
     
     n_layers = model.config.num_hidden_layers
-    #q_config = BaseQuantizeConfig(nbits=4, group_size=64) 
     
     for i in range(n_layers):
         if i < 4:
